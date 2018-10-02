e16278decd252b5dfd41a743f11e9e717f943e16
kyang19 <42977150+kyang19@users.noreply.github.com>
2018-10-02 15:33:54 -0400

    Add files via upload

---
+++ InputPractice.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class InputPractice 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		
+		
+		
+		System.out.print("Input the rectangle length:   ");
+		
+		double recLength = in.nextDouble();
+		in.nextLine();
+		
+		System.out.print("Input the rectangle width:   ");
+		double recWidth = in.nextDouble();
+		in.nextLine();
+		
+		System.out.printf("\nThe area of the rectangle is: %15.2f", recLength*recWidth);
+		System.out.printf("\nThe perimeter of the rectangle is: %10.2f", 2*recLength+ 2*recWidth);
+		double diagonal = Math.sqrt(recLength*recLength+recWidth*recWidth);
+		System.out.printf("\nThe diagonal length of the rectangle is: %.2f", diagonal);
+	}
+
+}
